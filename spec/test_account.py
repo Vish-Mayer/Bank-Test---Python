237d51c7e443e9d120717df9dddbdc484b4cfed3
Vish Mayer <vish.mayer@gmail.com>
2021-04-06 17:57:21 +0100

    account deposit

--- spec/test_account.py
+++ spec/test_account.py
@@ -6,3 +6,10 @@ class Bank_Account_Test(unittest.TestCase):
     def test_default_balance(self):
         self.account = Bank_Account()
         self.assertEqual(self.account.balance, 0)
+
+    def test_deposit(self):
+        self.account = Bank_Account()
+        self.account.deposit(10)
+        self.assertEqual(self.account.balance, 10)
+
+    
--- src/account.py
+++ src/account.py
@@ -4,3 +4,6 @@ class Bank_Account:
 
     def __init__(self, balance=0):
         self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
