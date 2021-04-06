41a0388a3726fdd5d1c86fbd3a30ba11b19cfa64
Vish Mayer <vish.mayer@gmail.com>
2021-04-06 18:08:23 +0100

    refactored account spec

--- spec/test_account.py
+++ spec/test_account.py
@@ -3,13 +3,12 @@ from src.account import Bank_Account
 
 class Bank_Account_Test(unittest.TestCase):
 
-    def test_default_balance(self):
+    def setUp(self):
         self.account = Bank_Account()
+
+    def test_default_balance(self):
         self.assertEqual(self.account.balance, 0)
 
     def test_deposit(self):
-        self.account = Bank_Account()
         self.account.deposit(10)
         self.assertEqual(self.account.balance, 10)
-
-    
