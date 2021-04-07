c8dcbed6b1cab83fa5525b78961381bdb04336fe
Vish Mayer <vish.mayer@gmail.com>
2021-04-07 15:09:37 +0100

    refactored tests

--- spec/test_transaction.py
+++ spec/test_transaction.py
@@ -1,3 +1,4 @@
+import pytest
 import unittest
 from unittest import mock
 from src.transaction import Transaction
@@ -7,11 +8,14 @@ class Transaction_Test(unittest.TestCase):
     def setUp(self):
         self.transaction = Transaction()
         self.transaction.transaction_record = mock.Mock(name="transaction_record")
+        self.transaction.history.clear()
 
     def test_credit(self):
         self.transaction.credit(0, 10)
         self.transaction.transaction_record.assert_called_once_with(balance=10, credit=10)
+        self.assertEqual(len(self.transaction.history), 1)
 
     def test_debit(self):
         self.transaction.debit(10, 5)
         self.transaction.transaction_record.assert_called_once_with(balance=5, debit=5)
+        self.assertEqual(len(self.transaction.history), 1)
